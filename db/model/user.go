743c54492a9cc9427ba6864625e5010d82c0f81d
Chocobone <yooho1030@naver.com>
2025-08-27 16:31:17 +0900

    feat(db): add mongoDB config and user model

---
+++ db/model/user.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID 		primitive.ObjectID	`bson:"_id,omitempty" json:"id,omitempty"`
+	UserID  int					`bson:"user_id" json:"user_id`
+	Name	string 				`bson:"name" json:"name"`
+	Email	string				`bson:"email" json:"email"`
+}
+
