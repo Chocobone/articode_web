6017b49f3e7092b2c915ead43496fab54b594af6
ParkJunHyung01 <mompu915@gmail.com>
2025-10-23 19:33:01 +0900

    3d modeling modify

--- main.go
+++ main.go
@@ -11,9 +11,15 @@ import (
 	httpSwagger "github.com/swaggo/http-swagger" //auto-API docs writter
 
 	dbConfig "github.com/Chocobone/articode_web/v2/db/config"
+
+	// ---- User API ---- //
 	"github.com/Chocobone/articode_web/v2/user"
 	userRepository "github.com/Chocobone/articode_web/v2/user/repository"
 
+	// ---- 3D API ---- //
+	modelingRepository "github.com/Chocobone/articode_web/v2/repository"
+	modelingService "github.com/Chocobone/articode_web/v2/service"
+	modelingHandler "github.com/Chocobone/articode_web/v2/handler"
 )
 
 func StatusHandler(c *gin.Context) {
@@ -58,11 +64,24 @@ func main() {
 	// Swagger
 	r.GET("/swagger/*any", gin.WrapH(httpSwagger.Handler()))
 
+	// User route
 	userRepo := userRepository.NewUserRepository()
-
 	userService := user.NewUserService(userRepo)
 	userHandler := user.NewUserHandler(userService)
 	user.RegisterUserRoutes(r, userHandler)
+
+	// 3D Modeling route
+	modelingCollection := db.Collection("modeling3d")
+	modelRepo := &modelingRepository.ModelingRepository{Collection: modelingCollection}
+	modelService := modelingService.NewModelingService(modelRepo)
+	modelHandler := modelingHandler.NewModelingHandler(modelService)
+
+	api3d := r.Group("/api/users/3d")
+	{
+		api3d.POST("", modelHandler.InsertModelHandler)     // 3D 모델 추가
+		api3d.GET("", modelHandler.GetModelListHandler)     // 3D 모델 목록
+		api3d.DELETE("/:id", modelHandler.DeleteModelHandler) // 3D 모델 삭제
+	}
 
 	// start the server
 	serverAddress := host + ":" + port
